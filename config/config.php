e47291c5939344cf87f6f84848b020424d69ca9a
JoeEverest <everestjoseph11@yahoo.com>
2020-03-13 18:18:16 +0300

    passchange for heroku test

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$loginUser = 'root';
-$loginPassword = '';
-$dbName = 'auth';
+$host = '3210jr44339.ipagemysql.com';
+$loginUser = 'joeeverest';
+$loginPassword = 'nGf2PKPThV*9';
+$dbName = 'regtest';
 $connect = mysqli_connect($host, $loginUser, $loginPassword, $dbName);//Connection variable
 
 if(mysqli_connect_errno()) 
