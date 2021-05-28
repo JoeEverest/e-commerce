52463725e5c9e805a402d6dcdec8215e7d4ce15a
JoeEverest <42711226+JoeEverest@users.noreply.github.com>
2021-05-28 14:08:29 +0300

    images on index as backgrounds some fullscreen tweak

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require("./config/session.php");
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
@@ -25,7 +26,7 @@ require("./config/session.php");
         </div>
         <?php if ($isLoggedIn) { ?>
             <span class="sell">
-                    <?php require("notifications.php"); ?>
+                <?php require("notifications.php"); ?>
                 <a href="/sell/">
                     <i class="fas fa-cart-plus"></i>
                 </a>
@@ -45,7 +46,8 @@ require("./config/session.php");
         ?>
             <div class="post card">
                 <h4><?php echo $name; ?></h4>
-                <img src="<?php echo $images[0]; ?>" alt="" loading="lazy">
+                <div class="img" style="
+    background-image: url('<?php echo $images[0]; ?>');"></div>
                 <div class="details">
                     <h5>
                         <span><?php echo number_format($price, 2); ?>/=</span>
